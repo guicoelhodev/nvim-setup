b3f1e8b32f9add6891de0ca0a69dbef12b9610c7
guicoelhodev <gs.coelho_dev@outlook.com>
2024-12-07 15:50:12 -0300

    feat(lazy): add clipboard

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
+vim.g.clipboard = "unnamedplus"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
