a97d0781c98a43947025b0c2242e0a5ba818714f
guicoelhodev <gs.coelho_dev@outlook.com>
2024-12-07 16:46:24 -0300

    feat: add lsp config session

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.signcolumn = 'yes'
+
