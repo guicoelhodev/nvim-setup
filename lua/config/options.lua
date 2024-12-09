3c74993b3477307a09e725aef589e083bb5c5019
guicoelhodev <gs.coelho_dev@outlook.com>
2024-12-09 13:31:14 +0000

    feat: add yank hightlight color mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,20 @@
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.breakindent = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
