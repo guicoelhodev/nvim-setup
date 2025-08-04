1bfe820e55c10b28d26b8ef5eb2c6b4d9e7aad48
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-03 21:33:10 -0300

    feat: add base neovim setup

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
+print('Hello new config')
+
+vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>e', ':Explore <CR>')
+vim.keymap.set('n', '<C-u>', ':so<CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+
+vim.pack.add({
+	{ src = 'https://github.com/folke/tokyonight.nvim'}
+})
+
+vim.cmd[[colorscheme tokyonight-night]]
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
