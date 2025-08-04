34e880322c539aac3bc5fef7fcbc8fbef40c7be4
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-03 21:57:55 -0300

    feat: add mason and mason-lsp

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-print('Hello new config')
+require('config.options')
 
 vim.g.mapleader = ' '
 
@@ -7,7 +7,25 @@ vim.keymap.set('n', '<C-u>', ':so<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 
 vim.pack.add({
-	{ src = 'https://github.com/folke/tokyonight.nvim'}
+	{ src = 'https://github.com/folke/tokyonight.nvim'},
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' }
 })
 
 vim.cmd[[colorscheme tokyonight-night]]
+
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+  ensure_installed = { 'lua_ls', 'ts_ls' },
+  automatic_enable = true,
+})
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true,
+    },
+})
+--vim.lsp.enable('lua_ls')
+
