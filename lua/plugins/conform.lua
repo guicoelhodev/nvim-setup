fd7220e16755996ae4d90bada622a005fd99ba64
guicoelhodev <gs.coelho_dev@outlook.com>
2026-03-17 15:51:28 -0300

    fix: add htmlangular to recognize prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,21 @@
 return {
 	{
-		'stevearc/conform.nvim',
+		"stevearc/conform.nvim",
 		config = function()
-			require('conform').setup({
+			require("conform").setup({
 				formatters_by_ft = {
-					javascript = { 'prettier' },
-					typescript = { 'prettier' },
-					javascriptreact = { 'prettier' },
-					typescriptreact = { 'prettier' },
-					css = { 'prettier' },
-					html = { 'prettier' },
-					json = { 'prettier' },
-					yaml = { 'prettier' },
-					markdown = { 'prettier' },
-					lua = { 'stylua' },
-				}
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					htmlangular = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					lua = { "stylua" },
+				},
 			})
 		end,
 	},
