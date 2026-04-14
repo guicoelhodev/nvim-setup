48b777e5148cbc3a0a21b89cf0b92f0769e5aa35
guicoehodev <gs.coelho_dev@outlook.com>
2026-04-14 16:44:34 -0300

    fix: fix angular issues
    Disable ts_ls when Angular.ts file is open

--- init.lua
+++ init.lua
@@ -1,9 +1,16 @@
 -- Load options first
-require('config.options')
+require("config.options")
 
 -- Bootstrap and setup lazy.nvim
-require('config.lazy')
+require("config.lazy")
 
 -- Load LSP and keymaps
-require('config.lsp')
-require('config.keymap')
+require("config.lsp")
+require("config.keymap")
+
+-- This prevent ts_ls and angular_ls access the same typescript file
+local angular_markers = vim.fs.find({ "angular.json", "nx.json" }, { upward = true })
+
+if #angular_markers > 0 then
+	vim.lsp.config("ts_ls", { autostart = false })
+end
