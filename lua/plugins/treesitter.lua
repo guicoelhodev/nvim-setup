3e01cc09ff3ebc095f7f4850567d0d5c08dc150e
guicoelhodev <gs.coelho_dev@outlook.com>
2026-04-28 14:40:28 -0300

    fix: update to use tree-sitter-manager

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,11 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {},
-				sync_install = false,
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+	"romus204/tree-sitter-manager.nvim",
+	dependencies = {},
+	config = function()
+		require("tree-sitter-manager").setup({
+			ensure_installed = {},
+			auto_install = true,
+			highlight = true,
+		})
+	end,
 }
